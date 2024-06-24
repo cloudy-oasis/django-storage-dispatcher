0d10e2eff399f757fcf1bb9b0c67e5fa8fbb943f
cloudy-oasis <98921062+cloudy-oasis@users.noreply.github.com>
2024-06-24 11:59:15 +0200

    Added a comment explaining missing tests

--- storage_dispatcher/tests.py
+++ storage_dispatcher/tests.py
@@ -1,1 +1,3 @@
-# Create your tests here.
+# Tests are not defined here, because they depend on the version of
+# Django being used (and we're testing half a dozen Django versions).
+# Check /tests instead!
