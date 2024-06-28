597e757dde9256c642b89b088578c0dfbc72cd38
cloudy-oasis <98921062+cloudy-oasis@users.noreply.github.com>
2024-06-28 11:32:52 +0200

    Changed distribution/import package names and bumped version

--- storage_dispatcher/apps.py
+++ storage_dispatcher/apps.py
@@ -3,6 +3,6 @@ from django.apps import AppConfig
 
 class StorageDispatcherConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "django_storage_dispatcher"
-    verbose_name = "Django Storage Dispatcher"
+    name = "storage_dispatcher"
+    verbose_name = "Storage Dispatcher"
     label = "storage_dispatcher"
